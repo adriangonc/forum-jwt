3ea52480f95dcfd08c14f949b23a095a41291bc0
adriangonc <adriangonc@yahoo.com.br>
2021-07-09 13:18:53 -0300

    Alterando aplicação para utilizar Pageable em consulta de cursos

--- src/main/java/br/com/jwt/forum/controller/TopicosController.java
+++ src/main/java/br/com/jwt/forum/controller/TopicosController.java
@@ -1,7 +1,6 @@
 package br.com.jwt.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -11,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,11 +43,9 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina,
-			@RequestParam int quantidade) {
-		
-		Pageable paginacao = PageRequest.of(pagina, quantidade);
-		
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+			@PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 1) Pageable paginacao) {
+
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(topicos);
