cf6f099e5b86f8a907071e217810358b238ae98d
adriangonc <adriangonc@yahoo.com.br>
2021-07-12 11:41:51 -0300

    Implementação de autenticação via JWT

---
+++ src/main/java/br/com/jwt/forum/config/security/TokenService.java
@@ -0,0 +1,48 @@
+package br.com.jwt.forum.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import br.com.jwt.forum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+	
+	@Value("${forum.jwt.expiration}")
+	private String expiration;
+	
+	@Value("${forum.jwt.secret}")
+	private String secret;
+
+	public String gerarToken(Authentication authentication) {
+		Usuario usuarioLogado = null;
+		try {
+			usuarioLogado = (Usuario) authentication.getPrincipal();
+		} catch (Exception e) {
+			System.out.println("Erro ao buscar usuário");
+			e.printStackTrace();
+		}
+		Date dataGeracaoToken = new Date();
+		Date dataExpiracao = new Date(dataGeracaoToken.getTime() + Long.parseLong(expiration));
+		
+		try {
+			return Jwts.builder()
+					.setIssuer("API cliente forum")
+					.setSubject(usuarioLogado.getId().toString())
+					.setIssuedAt(dataGeracaoToken)
+					.setExpiration(dataExpiracao)
+					.signWith(SignatureAlgorithm.HS256, secret)
+					.compact();
+		} catch (Exception e) {
+			System.out.println("Erro ao gerar token");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
