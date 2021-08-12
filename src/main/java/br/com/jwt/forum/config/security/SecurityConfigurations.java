76b3a2adb20421ffa794e279adbb940d9bbba4e0
Adriano Gonçalves de Souza <adriangonc@yahoo.com.br>
2021-08-12 18:16:59 -0300

    Configuração do swagger

--- src/main/java/br/com/jwt/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/jwt/forum/config/security/SecurityConfigurations.java
@@ -59,7 +59,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	/** Configurações de recursos staticos, imgs, js, css etc... **/
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configurations/**", "/swagger-resources/**");
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 	}
 	
 	//metodo para gerar hash da senha
--- src/main/java/br/com/jwt/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/jwt/forum/config/swagger/SwaggerConfigurations.java
@@ -1,10 +1,14 @@
 package br.com.jwt.forum.config.swagger;
 
+import java.util.Arrays;
+
 import br.com.jwt.forum.modelo.Usuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -16,6 +20,13 @@ public class SwaggerConfigurations {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select().apis(RequestHandlerSelectors.basePackage("br.com.jwt.forum"))
                 .paths(PathSelectors.ant("/**")).build()
-                .ignoredParameterTypes(Usuario.class);
+                .ignoredParameterTypes(Usuario.class)
+                .globalOperationParameters(Arrays.asList(new ParameterBuilder()
+                                .name("Authorization")
+                                .description("Header para envio de Token JWT")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build()));
     }
 }
